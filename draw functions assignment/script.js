6c145e51a9bb7ee12dcd625d30b6d481035265ae
forampatel2 <78555940+forampatel2@users.noreply.github.com>
2021-02-09 11:20:13 -0700

    Add files via upload

--- draw functions assignment/script.js
+++ draw functions assignment/script.js
@@ -6,63 +6,43 @@ cnv.height = 600;
 
 //Draw Targets
 
-function drawTarget(x, y, color) {
+function drawTarget(x, y, n, color) {
     stroke(color);
-    circle(x + 50, y + 250, 100, "stroke");
-    circle(x + 50, y + 250, 90, "stroke");
-    circle(x + 50, y + 250, 80, "stroke");
-    circle(x + 50, y + 250, 70, "stroke");
-    circle(x + 50, y + 250, 60, "stroke");
-    circle(x + 50, y + 250, 50, "stroke");
-    circle(x + 50, y + 250, 40, "stroke");
-    circle(x + 50, y + 250, 30, "stroke");
-    circle(x + 50, y + 250, 20, "stroke");
-    circle(x + 50, y + 250, 10, "stroke");
+    for (let numRings = 1; numRings <= n; numRings++ ){
+    circle(x + 50, y + 150, n * 10, "stroke");
+    circle(x + 300, y + 400 , n * 3, "stroke");
+    circle(x + 275, y + 350, n * 6, "stroke");
+    }
 }
-drawTarget(100, 100, "green");
+drawTarget(100, 100, 10, "green");
+drawTarget(300, 200, 3, "blue");
+drawTarget(275, 300, 6, "red");
 
-function drawTarget1(x, y, color) {
-    stroke(color);
-    circle(x + 300, y + 100, 80, "stroke");
-    circle(x + 300, y + 100, 60, "stroke");
-    circle(x + 300, y + 100, 30, "stroke");
-}
-drawTarget1(100, 100, "blue");
-
-function drawTarget2(x, y, color){
-    stroke(color);
-    circle(x + 275, y + 350, 50, "stroke");
-    circle(x + 275, y + 350, 40, "stroke");
-    circle(x + 275, y + 350, 30, "stroke");
-    circle(x + 275, y + 350, 20, "stroke");
-    circle(x + 275, y + 350, 10, "stroke");
-
-}
-drawTarget2(100, 100, "red");
 
 //Draw Platforms
 
-function drawStar(x, y, color){
-    stroke(color);
-    lineWidth(4);
+function drawStar(x, y, w, color){
+    for(let numWidth = 1; numWidth <=w; numWidth++){
+        stroke(color);
+    }
     line(x - 40, y + 90, x + 5, y + 20);
     line(x + 40, y + 90, x + 5, y + 20);
     line(x + 50, y + 50 , x - 40 , y + 90 );
     line(x - 50, y + 50 , x + 40 , y + 90 );
     line(x - 50 , y + 50 , x + 50 , y + 50);
 }
-drawStar(700, 150, "blue");
+drawStar(700, 150, 3, "blue");
 
-function drawStar2(x, y, color){
-    stroke(color);
-    lineWidth(2);
-    line(x - 60, y + 150, x + 8, y + 40);
-    line(x + 60, y + 150, x + 8, y + 40);
-    line(x + 80, y + 80 , x - 60 , y + 150 );
-    line(x - 80, y + 80 , x + 60 , y + 150 );
-    line(x - 80 , y + 80 , x + 80 , y + 80);
-}
-drawStar2(900, 100, "green");
+//function drawStar2(x, y, color){
+   // stroke(color);
+   // lineWidth(2);
+   // line(x - 60, y + 150, x + 8, y + 40);
+   // line(x + 60, y + 150, x + 8, y + 40);
+   // line(x + 80, y + 80 , x - 60 , y + 150 );
+   // line(x - 80, y + 80 , x + 60 , y + 150 );
+   // line(x - 80 , y + 80 , x + 80 , y + 80);
+//}
+//drawStar2(900, 100, "green");
 
 
 
